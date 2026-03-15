89337025e972efd3c474b4bc38415d57b52cc3cb
joel5959 <joel.williams@dcmail.ca>
2026-03-15 06:17:50 -0400

    Added information to the Homepage

--- portfolio-project/src/App.jsx
+++ portfolio-project/src/App.jsx
@@ -1,24 +1,20 @@
 import Contact from "./pages/Contact.jsx";
-import About from "./pages/About.jsx";
 import Projects from "./pages/Projects.jsx";
 import NavBar from "./ui/NavBar.jsx"
 import Footer from "./ui/Footer.jsx";
-import Header from "./ui/Header.jsx";
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 
 function App() {
     return (
         <div className="app-container">
-            <NavBar links={['Home', 'About', 'Contact', 'Projects']}/>
+            <NavBar links={['Home', 'Projects', 'Contact']}/>
 
             <Routes>
                 <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
                 <Route path="/projects" element={<Projects />}/>
                 <Route path="/contact" element={<Contact/>}/>
             </Routes>
-
 
             <Footer/>
         </div>
--- portfolio-project/src/pages/Home.jsx
+++ portfolio-project/src/pages/Home.jsx
@@ -3,7 +3,14 @@ function Home(){
     return(
 
         <section className="card">
-            <h1>Joel Williams</h1>
+            <h1>About Joel Williams</h1>
+
+            <p>Hi, I’m Joel, a Full-Stack Developer and current Computer Programming & Analysis student at Durham College.
+                With a 3.92 GPA and a passion for clean, efficient code, I specialize in building secure web applications
+                and interactive mobile experiences. I enjoy bridging the gap between complex back-end logic and intuitive
+                front-end design using tools like React, Next.js, and Flutter.
+            </p>
+
         </section>
 
     );
