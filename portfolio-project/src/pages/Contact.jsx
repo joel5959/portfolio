f4f123baedd751ded7cf988868d9ab4f09b1bd83
joel5959 <joel.williams@dcmail.ca>
2026-03-16 07:54:16 -0400

    Started on the Contact Page

--- portfolio-project/src/pages/Contact.jsx
+++ portfolio-project/src/pages/Contact.jsx
@@ -1,12 +1,59 @@
-function Contact (){
+import {useState} from "react";
 
-    return(
+function Contact() {
+
+    const [error, setError] = useState("");
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setError("");
+
+        setEmail("");
+        setName("");
+        setMessage("");
+    }
+
+    return (
         <section>
             <h1> Contact Me </h1>
+
+            <div>
+                <input
+                    type="name"
+                    placeholder="Enter your name"
+                    value={name}
+                    name="name"
+                />
+            </div>
+            <div>
+                <input
+                    type="email"
+                    placeholder="Enter your email address"
+                    value={email}
+                    name="email"
+                />
+            </div>
+            <div>
+                <input
+                    type="message"
+                    placeholder="Enter your message"
+                    value={message}
+                    name="message"
+                />
+            </div>
+            <div>
+                <button>
+                    Submit
+                </button>
+            </div>
+
         </section>
 
     );
-    
+
 }
 
 
